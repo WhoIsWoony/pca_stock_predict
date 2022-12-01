9d00cd65eefc6b9cfa720d3bda3f0c11cf0f71ff
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-02 03:59:55 +0900

    feat: change data crawling path

--- data/train/etf_read.py
+++ data/train/etf_read.py
@@ -2,8 +2,8 @@ import FinanceDataReader as fdr
 #Read list(symbol, name) of etf
 #Load and write the one-year closing price of each stock
 
-PATH_COMPANY_CODE = "./data/dataset_input/etf_list.txt" #(회사명 주식코드)로 이루어진 파일
-PATH_CLOSING_PRICE_OF_1YEAR = "./data/dataset_input/etf_cp_list.txt" #(584개의 회사에 대한 1년 개장일 248일 종가)로 이루어진 파일
+PATH_COMPANY_CODE = "./data/train/etf_list.txt" #(회사명 주식코드)로 이루어진 파일
+PATH_CLOSING_PRICE_OF_1YEAR = "./data/train/etf_cp_list.txt" #(584개의 회사에 대한 1년 개장일 248일 종가)로 이루어진 파일
 
 fr = open(PATH_COMPANY_CODE, "r", encoding='euc-kr')
 fw = open(PATH_CLOSING_PRICE_OF_1YEAR, 'w', encoding='euc-kr')
--- data/train/krx300_read.py
+++ data/train/krx300_read.py
@@ -2,8 +2,8 @@ import FinanceDataReader as fdr
 #Read list(symbol, name) of krx300
 #Load and write the one-year closing price of each stock
 
-PATH_COMPANY_CODE = "./data/dataset_input/krx300_list.txt" #(회사명 주식코드)로 이루어진 파일
-PATH_CLOSING_PRICE_OF_1YEAR = "./data/dataset_input/krx300_cp_list.txt" #(584개의 회사에 대한 1년 개장일 248일 종가)로 이루어진 파일
+PATH_COMPANY_CODE = "./data/train/krx300_list.txt" #(회사명 주식코드)로 이루어진 파일
+PATH_CLOSING_PRICE_OF_1YEAR = "./data/train/krx300_cp_list.txt" #(584개의 회사에 대한 1년 개장일 248일 종가)로 이루어진 파일
 
 fr = open(PATH_COMPANY_CODE,'r')
 fw = open(PATH_CLOSING_PRICE_OF_1YEAR, 'w')
