8abd36cadb96dcb4e8d78789e82f9ca50c84bcf3
WhoIsWoony <WhoIsWoony@gmail.com>
2022-12-02 03:24:58 +0900

    feat: add dataset

---
+++ data/train/krx300_read.py
@@ -0,0 +1,23 @@
+import FinanceDataReader as fdr
+#Read list(symbol, name) of krx300
+#Load and write the one-year closing price of each stock
+
+PATH_COMPANY_CODE = "./data/dataset_input/krx300_list.txt" #(회사명 주식코드)로 이루어진 파일
+PATH_CLOSING_PRICE_OF_1YEAR = "./data/dataset_input/krx300_cp_list.txt" #(584개의 회사에 대한 1년 개장일 248일 종가)로 이루어진 파일
+
+fr = open(PATH_COMPANY_CODE,'r')
+fw = open(PATH_CLOSING_PRICE_OF_1YEAR, 'w')
+
+while True:
+    line = fr.readline()
+    if not line: break
+
+    symbol = line.split(' ')[0]
+    cp_oneyear = fdr.DataReader(symbol, '2020-01-01', '2020-12-31').Close
+
+    for cp_day in cp_oneyear:
+        fw.write(str(cp_day)+" ")
+    fw.write("\n\n")
+        
+fr.close()
+fw.close()
